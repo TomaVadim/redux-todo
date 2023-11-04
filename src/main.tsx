219f18d298398ecb8c4856cd0aa946971ba25ab3
VadimToma <https://github.com/TomaVadim>
2023-11-04 18:08:16 +0200

    chore: move styles imports to theme

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,6 @@ import { App } from "@/App";
 
 import { generateTheme } from "@/theme/theme";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 const root = document.getElementById("root") as HTMLDivElement;
 
 ReactDOM.createRoot(root).render(
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,17 @@
 import { createTheme } from "@mui/material";
 import { Theme } from "@mui/material";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 export const generateTheme = (): Theme => {
   const theme = createTheme({
     palette: {},
-    typography: {},
+    typography: {
+      fontFamily: ["Roboto", "sans-serif"].join(","),
+    },
   });
 
   return theme;
